6e4bea837c91dd9f96ca91a603403a006ab6f06b
Samar2410 <samarvirnarula@gmail.com>
2023-02-03 01:07:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import TextForm from "./Components/TextForm";
+import Navbar from "./Components/Navbar";
+import About from './Components/About';
+import {useState} from 'react';
+import Alert from './Components/Alert';
+// import { BrowserRouter as Router,
+// Route, Routes , Link } from 'react-router-dom';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [mode,setMode]=useState("light");
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 3000);
+  }
+
+  // const removeBodyClasses=()=>{
+  //   document.body.classList.remove("bg-danger");
+  //   document.body.classList.remove("bg-primary");
+  //   document.body.classList.remove("bg-warning");
+  //   document.body.classList.remove("bg-success");
+  //   document.body.classList.remove("bg-dark");
+  //   document.body.classList.remove("bg-light");
+  // }
+  const toggleMode=(cls)=>{
+    // removeBodyClasses();
+    // document.body.classList.add("bg-"+cls)
+    if(mode==="dark"){
+      setMode("light");
+      document.body.style.backgroundColor="white";
+      showAlert("Light mode has been enabled","success");
+      document.title="TextUtils-Light mode";
+    }
+    else{
+      setMode("dark");
+      document.body.style.backgroundColor="#37373c";
+      showAlert("Dark mode has been enabled","success");
+      document.title="TextUtils-Dark mode";
+    } 
+  }
+ return (
+ <>
+  {/* <Router> */}
+<Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
+{/* <Navbar /> */}
+
+<div className="container my-3">
+
+{/* <Routes>
+<Route exact path="/about" element={<About  mode={mode}  />}>
+            </Route>
+         
+          <Route exact path="/" element={ <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/> }>
+          
+          </Route>
+        </Routes>
+  */}
+  {/* <About/> */}
+  {/* <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/> */}
+  <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>
+</div>
+ {/* </Router> */}
+  </>
+ 
+  
+ 
+  
   );
 }
 
